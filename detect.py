449eccaa92194ecf9ea3687bd2c6e826102583ba
Ubuntu <ubuntu@ip-172-31-81-229.ec2.internal>
2019-04-22 23:07:51 +0000

    Integrated wfuzz

--- detect.py
+++ detect.py
@@ -11,49 +11,49 @@ def find_word(w):
 def keyword_scan(url, cookies, headers, text):
     # TODO: Implement fast pattern matching
     # https://yacin.nadji.us/classes/f16-adv-comp-sec/papers/15-zozzle.pdf
-    result = [] # (type, name, version)
+    result = set() # (type, name, version)
 
     # Server features -> nmap
     header_server_string = headers['Server']
     if find_word('apache')(header_server_string) is not None:
         server_suffix = header_server_string.split("/")[-1].split(" ")
-        result.append(("server", "apache", server_suffix[0]))
+        result.add(("server", "apache", server_suffix[0]))
         if len(server_suffix) > 1:
-            result.append(("os", server_suffix[1].strip("()").lower(), ""))
+            result.add(("os", server_suffix[1].strip("()").lower(), ""))
     
     # Application features
     if find_word('wordpress')(text) is not None:
-        result.append(("app", "wordpress", ""))
+        result.add(("app", "wordpress", ""))
         # -> wpscan
 
     if find_word('php-nuke')(text) is not None:
-        result.append(("app", "php-nuke", ""))
+        result.add(("app", "php-nuke", ""))
 
     # Database features -> sqlmap
     if find_word('mysql')(text) is not None:
-        result.append(("db", "mysql", ""))
+        result.add(("db", "mysql", ""))
 
     if find_word('postgresql')(text) is not None:
-        result.append(("db", "postgresql", ""))
+        result.add(("db", "postgresql", ""))
 
     # OS features -> nmap
     if find_word('mac os')(text) is not None:
-        result.append(("os", "mac_os", ""))
+        result.add(("os", "mac_os", ""))
 
     if find_word('windows')(text) is not None:
-        result.append(("os", "windows", ""))
+        result.add(("os", "windows", ""))
 
     if find_word('linux')(text) is not None:
-        result.append(("os", "linux", ""))
+        result.add(("os", "linux", ""))
 
     if find_word('ubuntu')(text) is not None:
-        result.append(("os", "linux", "ubuntu"))
+        result.add(("os", "linux", "ubuntu"))
 
     if find_word('fedora')(text) is not None:
-        result.append(("os", "linux", "fedora"))
+        result.add(("os", "linux", "fedora"))
 
     if find_word('centos')(text) is not None:
-        result.append(("os", "linux", "centos"))
+        result.add(("os", "linux", "centos"))
 
     return result
 
@@ -70,7 +70,7 @@ def nmap_scan(url):
         port = host_port[1]
 
     nmap_result = subprocess.check_output(["nmap", "-p", port, "-sV", host, "-oG", "-"])
-    output = {}
+    output = set()
     for line in nmap_result.decode("utf-8").split("\n"):
         if len(line) > 0 and line.strip(" \t")[0] != '#':
             for field in line.split("\t"):
@@ -78,22 +78,75 @@ def nmap_scan(url):
                 key = key_value[0]
                 value = key_value[1].strip(" ")
                 if key == "Host":
-                    output["ip_addr"] = value.split(" ")[0]
-                    output["reverse_hostname"] = value.split(" ")[1].strip("()")
+                    # output["ip_addr"] = value.split(" ")[0]
+                    # output["reverse_hostname"] = value.split(" ")[1].strip("()")
+                    pass
                 elif key == "Ports":
                     port_fields = value.split("/")
                     if port_fields[0] == port:
-                        output["server"] = port_fields[6]
+                        output.add(("server", port_fields[6], ""))
 
     return output
 
+def wfuzz_scan(url):
+    wfuzz_result = subprocess.check_output(["wfuzz", "-w", "data/wfuzz_list.txt", "--hc", "404", url + ("/FUZZ" if url[-1] != "/" else "FUZZ")])
+    output = set()
+    for line in wfuzz_result.decode("utf-8").split("\n"):
+        fields = line.split("\t")
+        if len(fields) == 4:
+            path = fields[-1].strip(" \t").strip("\"")
+            if path == "SiteServer":
+                output.add(("server", "iis", ""))
+            elif path[:5] == "W3SVC":
+                output.add(("server", "iis", ""))
+            elif path[:5] == "WEB-INF":
+                output.add(("language", "java", ""))
+            elif path == "apache":
+                output.add(("server", "apache", ""))
+            elif path == "asp":
+                output.add(("language", "asp", ""))
+            elif path == "aspadmin":
+                output.add(("language", "asp", ""))
+            elif path == "cfdocs":
+                output.add(("language", "cfm", ""))
+            elif path == "dbase":
+                output.add(("db", "dbase", ""))
+            elif path == "dev60cgi":
+                output.add(("db", "oracle", ""))
+            elif path == "docs41":
+                output.add(("language", "java", ""))
+            elif path == "docs51":
+                output.add(("language", "java", ""))
+            elif path == "iis":
+                output.add(("server", "iis", ""))
+            elif path == "jdbc":
+                output.add(("language", "java", ""))
+            elif path == "jsp":
+                output.add(("language", "java", ""))
+            elif path == "oradata":
+                output.add(("db", "oracle", ""))
+            elif path == "phpmyadmin":
+                output.add(("language", "php", ""))
+            elif path == "phpMyAdmin":
+                output.add(("language", "php", ""))
+            elif path == "index.asp":
+                output.add(("language", "asp", ""))
+            elif path == "index.aspx":
+                output.add(("language", "asp", ""))
+            elif path == "index.php":
+                output.add(("language", "asp", ""))
+            elif path == "index.jsp":
+                output.add(("language", "asp", ""))
+            elif path == "index.cfm":
+                output.add(("language", "cfm", ""))
+    return output
 
 def process_response(response):
     print("apparent_encoding: " + r.apparent_encoding)
     # print("content: " + r.content.decode(r.apparent_encoding))
     cookie_output = []
     for elem in r.cookies.items():
-        cookie_output.append("\t" + elem[0] + "=" + elem[1])
+        cookie_output.add("\t" + elem[0] + "=" + elem[1])
     print("cookies: " + "\n".join(cookie_output))
     print("elapsed: " + str(r.elapsed.total_seconds()))
     print("encoding: " + r.encoding)
@@ -112,8 +165,14 @@ def process_response(response):
     print("=" * 80)
 
     nmap_result = nmap_scan(r.url)
-    for k, v in nmap_result.items():
-        print(k + "=" + v)
+    for keyword in nmap_result:
+        print(" ".join(keyword))
+    print("=" * 80)
+
+    wfuzz_result = wfuzz_scan(r.url)
+    for keyword in wfuzz_result:
+        print(" ".join(keyword))
+    print("=" * 80)
 
     # TODO: (1) compare keywords and nmap_result, find inconsistence (if any)
     #        (2) launch specialized scanners (e.g. wpscan)
